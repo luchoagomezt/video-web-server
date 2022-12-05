4811e29f52db970b5dea5ba900fb566c98359cb1
Luis Alfonso Gomez Tinoco <luchoagomezt@gmail.com>
2022-12-05 01:24:30 -0500

    Refactoring.. decoupling template from object Test

--- web_server/views.py
+++ web_server/views.py
@@ -1,7 +1,10 @@
 from flask import request, redirect, url_for, render_template, flash, session
 from web_server import app
-
+from web_server.scripts.test import Test
 
 @app.route('/')
 def show_entries():
-    return render_template("index.html")
+    test = Test("MEDQA 306 Modify and save professional s file")
+    video = test.get_video_file_name()
+    track = test.get_track_file_name()
+    return render_template("index.html", video_name=video, track_name=track)
