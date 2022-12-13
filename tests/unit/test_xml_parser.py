5874ce845d5dac0435c1a457bf8160d9d71d453d
Luis A Gomez-Tinoco <luchoagomezt@gmail.com>
2022-12-13 10:32:55 -0500

    Fixing file path

--- tests/unit/test_xml_parser.py
+++ tests/unit/test_xml_parser.py
@@ -1,9 +1,13 @@
 from web_server.entity.xml_parser import XMLParser
 import untangle
+import os
 
 
 def test_generate_vtt():
-    robot_xml_object = untangle.parse("original.xml")
+    config_directory = os.path.dirname(os.path.realpath(__file__))
+    xml_file = os.path.join(os.path.dirname(config_directory), "unit/original.xml")
+
+    robot_xml_object = untangle.parse(xml_file)
     xml_parser = XMLParser(robot_xml_object)
     vtt = xml_parser.generate_vtt("MEDQA 339 Add a professional")
 
@@ -14,3 +18,7 @@ def test_generate_vtt():
     assert("19" in vtt[20])
     assert("00:00:58.533 --> 00:01:02.657" in vtt[20])
     assert("Cleanup" in vtt[20])
+
+    for vtt_entry in xml_file:
+        pass
+
