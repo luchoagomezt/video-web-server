f55a8ffc0cf56561819d3d0fc2d8b692827c9cb7
Luis A Gomez-Tinoco <luchoagomezt@gmail.com>
2022-12-13 10:44:41 -0500

    Fixing lint errors

--- tests/unit/test_xml_parser.py
+++ tests/unit/test_xml_parser.py
@@ -11,14 +11,10 @@ def test_generate_vtt():
     xml_parser = XMLParser(robot_xml_object)
     vtt = xml_parser.generate_vtt("MEDQA 339 Add a professional")
 
-    assert(len(vtt) == 21)
-    assert("0" in vtt[1])
-    assert("00:00:00.000 --> 00:00:09.004" in vtt[1])
-    assert("Login as Super Administrator" in vtt[1])
-    assert("19" in vtt[20])
-    assert("00:00:58.533 --> 00:01:02.657" in vtt[20])
-    assert("Cleanup" in vtt[20])
-
-    for vtt_entry in xml_file:
-        pass
-
+    assert len(vtt) == 21
+    assert "0" in vtt[1]
+    assert "00:00:00.000 --> 00:00:09.004" in vtt[1]
+    assert "Login as Super Administrator" in vtt[1]
+    assert "19" in vtt[20]
+    assert "00:00:58.533 --> 00:01:02.657" in vtt[20]
+    assert "Cleanup" in vtt[20]
