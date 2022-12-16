d106cb7b1a122ed2767efad8ad262f7898399e73
Alex Lin <alex.lin@telus.com>
2022-12-16 13:02:04 -0500

    Dynamic file paths and test names

--- web_server/__init__.py
+++ web_server/__init__.py
@@ -4,8 +4,8 @@ import os
 app = Flask(__name__)
 app.config.from_object('web_server.config')
 
-APP_ROOT = os.path.dirname(os.path.abspath(__file__))
-UPLOAD_FOLD = "C:/git/fork-video-web-server/web_server/model"
+APP_ROOT = os.path.dirname(os.path.realpath(__file__))
+UPLOAD_FOLD = "model"
 UPLOAD_FOLDER = os.path.join(APP_ROOT, UPLOAD_FOLD)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
