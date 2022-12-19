5e3af648f51b106ad23b064b197d93ed54f45f90
Alex Lin <alex.lin@telus.com>
2022-12-19 11:50:58 -0500

    Update views.py

--- web_server/view/views.py
+++ web_server/view/views.py
@@ -8,34 +8,12 @@ VIDEO_FILE_NAME = "videos/<test case name>.webm"
 VTT_FILE_NAME = "videos/<test case name>.vtt"
 VTT_VIDEO = VTTVideo()
 
-<<<<<<< HEAD
-@app.route('/', methods = ['POST'])
-def upload_file():
-    if request.method == 'POST':
-        f = request.files['xml file']
-        f.save(os.path.join(app.config['UPLOAD_FOLDER'], f.filename))
-        return show_video_with_track(f.filename)
-
-@app.route('/')
-def show_video_with_track(file_name="original.xml"):
-    xml_file_name = file_name
-
-    xml_file_path_name = os.path.join(os.path.dirname(BASE_DIRECTORY), f'model\{xml_file_name}')
-    xml_object = untangle.parse(xml_file_path_name)
-    xml_parser = XMLParser(xml_object)
-
-    test_names = xml_parser.get_test_names()
-    test_case_name = test_names[0]
-
-    vtt = xml_parser.generate_vtt(test_case_name)
-=======
 
 @app.route('/select-test', methods = ['POST'])
 def select_test():
     app.logger.debug(f"select_test: {request.values['test-case-name']}")
 
     test_case_name = request.values['test-case-name']
->>>>>>> d884849bc68b7392ffe4f866ebda3c8351c32a3b
     vtt_file = os.path.join(os.path.dirname(BASE_DIRECTORY), "static/" +
                             VTT_FILE_NAME.replace("<test case name>", test_case_name))
 
