258dd47ffa3c043007f87b5c6a6bb6786ece0286
Alex Lin <alex.lin@telus.com>
2022-12-20 14:33:13 -0500

    Update uploading file style

--- web_server/static/main.js
+++ web_server/static/main.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
 				$('input[type="file"]').change(function(e) {
-  					var i = $(this).prev('label').clone();
   					var file = e.target.files[0].name;
   					$(this).prev('label').text(file);
  				});
