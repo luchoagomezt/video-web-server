b904d2d249ff2345686c392d65b7a5f24adfc592
Luis A Gomez-Tinoco <luchoagomezt@gmail.com>
2022-12-19 19:25:09 -0500

    Fixing issues with empty file name and test name.

--- web_server/entity/vtt_video.py
+++ web_server/entity/vtt_video.py
@@ -1,5 +1,8 @@
+from urllib3.exceptions import NewConnectionError
+
 from web_server.entity.xml_parser import XMLParser
 import untangle
+import requests
 
 
 class VTTVideo:
@@ -8,6 +11,7 @@ class VTTVideo:
         self.test_case_name = ""
         self.test_case_names = []
         self.vtt_list = []
+        self.video_url = ""
 
     def set_parser(self, file_name):
         self.parser = XMLParser(untangle.parse(file_name))
@@ -28,6 +32,27 @@ class VTTVideo:
 
     def get_vtt_list(self):
         return self.vtt_list
+
+    def set_video_url(self):
+        if self.parser is not None and self.test_case_name != "":
+            self.video_url = self.parser.get_video_url(self.test_case_name)
+
+    def save_video_to_file(self, video_file_name):
+        status = 500
+
+        if self.video_url != "":
+            try:
+                response = requests.get(self.video_url)
+            except Exception:
+                return status
+
+            with open(video_file_name, 'wb') as fp:
+                for chunk in response.iter_content(chunk_size=128):
+                    fp.write(chunk)
+
+            return response.status_code
+
+        return status
 
     def save_vtt_list_to_file(self, vtt_file_name):
         with open(vtt_file_name, 'w') as fp:
